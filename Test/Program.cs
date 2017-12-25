ec84857ab55ca6547313513a28ac10d24ebbd51b
Udo Wagner <udowagner@hotmail.de>
2017-12-25 14:51:11 +0100

    Added some files

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+	    delegate string myDelegate(int x);
+        static void Main(string[] args)
+        {
+		myDelegate myfunc = q => {
+			string retVal = "My Num = " + q;
+			return retVal;
+		};
+            Console.WriteLine("Hello World! " + myfunc(Convert.ToInt32(args[0])));
+        }
+    }
+}
